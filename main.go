a2e7151cab87f84b8a79b130d9fafe7c36283fb8
Lifon <lifon.info@gmail.com>
2025-05-02 21:28:49 +0600

    taking input

--- main.go
+++ main.go
@@ -12,4 +12,26 @@ func main() {
 	}
 	fmt.Println("Sum:", sum)
 	fmt.Println("length of sum: ", len(nums))
+	if isLeapYear() {
+		fmt.Println("Leap Year")
+	} else {
+		fmt.Println("Not a Leap Year")
+	}
+
+}
+
+func isLeapYear() bool {
+	var year int
+	fmt.Println("Enter a year:")
+	fmt.Scanln(&year)
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				return true
+			}
+			return false
+		}
+		return true
+	}
+	return false
 }
